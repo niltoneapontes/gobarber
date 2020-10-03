58314ed94fabc1387e56fcda2d612a38d13074fb
Nilton Pontes <niltoneapontes@gmail.com>
2020-10-03 17:56:04 -0300

    Conserto de bugs e eager loading

--- backend/src/modules/Appointments/services/ListProviderAppointmentsService.ts
+++ backend/src/modules/Appointments/services/ListProviderAppointmentsService.ts
@@ -2,6 +2,7 @@ import 'reflect-metadata';
 import { inject, injectable } from 'tsyringe';
 
 import ICacheProvider from '@shared/container/providers/CacheProvider/models/ICacheProvider';
+import { classToClass } from 'class-transformer';
 import Appointment from '../infra/typeorm/entities/Appointment';
 import IAppointmentsRepository from '../repositories/IAppointmentsRepository';
 
@@ -44,9 +45,7 @@ class ListProviderAppointmentsService {
         },
       );
 
-      console.log('Buscou do banco');
-
-      await this.cacheProvider.save(cacheKey, appointments);
+      await this.cacheProvider.save(cacheKey, classToClass(appointments));
     }
 
     return appointments;
